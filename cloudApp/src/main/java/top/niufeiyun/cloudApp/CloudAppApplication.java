14b3bf9fb39e19de9c2902ebb96ac2f0c7dcebf7
niufeiyun <niufeiyun@qq.com>
2019-02-20 18:37:48 +0800

    vison 1.0

---
+++ cloudApp/src/main/java/top/niufeiyun/cloudApp/CloudAppApplication.java
@@ -0,0 +1,14 @@
+package top.niufeiyun.cloudApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudAppApplication.class, args);
+	}
+
+}
+
